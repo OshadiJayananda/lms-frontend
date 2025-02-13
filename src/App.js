9636ed6dbdce76fff6faedc48dd9bccdf7071cdc
OshadiJayananda <158675994+OshadiJayananda@users.noreply.github.com>
2025-02-13 13:52:59 +0530

    Add Books to side bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Import necessary routing components
-import { ToastContainer } from 'react-toastify'; // Import ToastContainer
-import Home from './Components/Home';
-import Login from './Components/Login';
-import SignIn from './Components/SignIn';
-import Dashboard from './User/Dashboard';
-import SideBar from './Components/SideBar';
-import Categories from './Admin/Categories';
-import AdminDashboard from './Admin/AdminDashboard';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // Import necessary routing components
+import { ToastContainer } from "react-toastify"; // Import ToastContainer
+import Home from "./Components/Home";
+import Login from "./Components/Login";
+import SignIn from "./Components/SignIn";
+import Dashboard from "./User/Dashboard";
+import SideBar from "./Components/SideBar";
+import Categories from "./Admin/Categories";
+import AdminDashboard from "./Admin/AdminDashboard";
+import Books from "./Admin/Books";
 
 function App() {
   return (
@@ -14,15 +15,14 @@ function App() {
       <div>
         <ToastContainer />
         <Routes>
-          
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signIn" element={<SignIn />} />
           <Route path="/sideBar" element={<SideBar />} />
           <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/adminDashboard" element={<AdminDashboard/>} />
+          <Route path="/adminDashboard" element={<AdminDashboard />} />
           <Route path="/categories" element={<Categories />} />
-
+          <Route path="/books" element={<Books />} />
         </Routes>
       </div>
     </Router>
