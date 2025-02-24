1297157b6cc33b33c0565e487b297fcd9ee62b5c
OshadiJayananda <158675994+OshadiJayananda@users.noreply.github.com>
2025-02-24 14:27:13 +0530

    add sidebar to admin

--- src/Admin/AdminDashboard.js
+++ src/Admin/AdminDashboard.js
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { useState } from "react";
+import SideBar from "../Components/SideBar";
+import HeaderBanner from "./components/HeaderBanner";
 
 function AdminDashboard() {
+  const [isSidebarCollapsed, setSidebarCollapsed] = useState(false);
+
+  const heading_pic = process.env.PUBLIC_URL + "/images/heading_pic.jpg";
+
+  const handleToggle = () => {
+    setSidebarCollapsed(!isSidebarCollapsed);
+  };
+
   return (
     <div>
-      <h1>Admin Dashboard</h1>
-    </div>
-  )
-}
+      <SideBar isCollapsed={isSidebarCollapsed} onToggle={handleToggle} />
+      <div
+        style={{
+          marginLeft: isSidebarCollapsed ? "5%" : "20%",
+          padding: "0px",
+          transition: "margin-left 0.3s ease",
+        }}
+      >
+        <HeaderBanner book={"Book Section"} heading_pic={heading_pic} />
 
-export default AdminDashboard
+        <div style={{ padding: "20px" }}>
+          <h2>Welcome to the Dashboard</h2>
+          {/* Add additional dashboard content here */}
+        </div>
+      </div>
+    </div>
+  );
+}
+export default AdminDashboard;
