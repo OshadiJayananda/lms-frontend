ed8680e74c52bb50e00820d8efbf680e5ada21b3
OshadiJayananda <158675994+OshadiJayananda@users.noreply.github.com>
2025-03-17 21:50:35 +0530

    add hooks

--- src/hooks/useBackButtonHandler.js
+++ src/hooks/useBackButtonHandler.js
@@ -9,14 +9,34 @@ function useBackButtonHandler() {
     const handleBackButton = (event) => {
       const isAdmin = localStorage.getItem("admin_token");
       const isUser = localStorage.getItem("token");
+      const userRole = localStorage.getItem("role");
 
-      // If the user is on a non-dashboard page and clicks back, redirect to the dashboard
-      if (isAdmin && !location.pathname.startsWith("/admin/dashboard")) {
-        navigate("/admin/dashboard");
+      // Block back navigation to the login page if the user is authenticated
+      if (
+        (isAdmin || isUser) &&
+        (location.pathname === "/login" || location.pathname === "/signIn")
+      ) {
         event.preventDefault(); // Prevent default back navigation
-      } else if (isUser && !location.pathname.startsWith("/dashboard")) {
-        navigate("/dashboard");
-        event.preventDefault(); // Prevent default back navigation
+        if (userRole === "admin") {
+          navigate("/admin/dashboard", { replace: true }); // Redirect to admin dashboard
+        } else if (userRole === "user") {
+          navigate("/dashboard", { replace: true }); // Redirect to user dashboard
+        }
+      }
+
+      // Redirect to the appropriate dashboard if the user tries to go back from a non-dashboard page
+      if (
+        isAdmin &&
+        userRole === "admin" &&
+        !location.pathname.startsWith("/admin")
+      ) {
+        navigate("/admin/dashboard", { replace: true }); // Replace the current history entry
+      } else if (
+        isUser &&
+        userRole === "user" &&
+        !location.pathname.startsWith("/dashboard")
+      ) {
+        navigate("/dashboard", { replace: true }); // Replace the current history entry
       }
     };
 
