07a93836bee9779afddf5f4db7d133ffa1eeca6b
OshadiJayananda <158675994+OshadiJayananda@users.noreply.github.com>
2025-04-28 15:16:00 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -104,7 +104,7 @@ function App() {
             }
           />
           <Route
-            path="/borrowedHistory"
+            path="/admin/borrowed-history"
             element={
               <AdminAuthMiddleware>
                 <BorrowedHistory />
