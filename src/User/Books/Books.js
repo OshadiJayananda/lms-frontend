2ccb4c0a775d7e1253939ca689e2a6951998aebf
OshadiJayananda <158675994+OshadiJayananda@users.noreply.github.com>
2025-05-23 10:38:57 +0530

    change no of columns in the book page

--- src/User/Books/Books.js
+++ src/User/Books/Books.js
@@ -376,7 +376,7 @@ function Books() {
             </div>
           ) : (
             <>
-              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6">
                 {filteredBooks.map((book) => {
                   const isBorrowed =
                     Array.isArray(borrowedBooks) &&
